8967b8ad8bbf8542e018b7fdd52ede088384762c
Steven Adi Putra <154960387+stevennadii@users.noreply.github.com>
2024-11-04 11:48:06 +0700

    Add files via upload

---
+++ resources/js/pages/helpers/shared_value.js
@@ -0,0 +1,24 @@
+export let checkConsole = "Y";
+export const SRV_URL = "http://b2b_api.test/api/";
+
+export const NAMA_API_PELABUHAN_ASAL =
+    "$2y$10$MdKPgkUcZcsM2ZubeUdcmuSdwNS1e5W3BG/HK2CscWi1iXPXIPM5y";
+export const NAMA_API_TUJUAN =
+    "$2y$10$iFbF7EerSs7N5nD1Q/3IP.y1ctUJk9LT/A5ePRIifjQb8NXHazhyi";
+export const NAMA_API_PENGIRIMAN =
+    "$2y$10$B3vymWfYhVU3kitgkgTFGOZ0medEyyNyyvU7Aa9Fzuyz3AwbAGbre";
+export const NAMA_API_MATA_UANG =
+    "$2y$10$NvcPtlSbISucZ2wUqD5F3eH/JsVpznWxI8mykOFyqWPrzq9Jmc3IC";
+export const NAMA_API_PROFIL_PERUSAHAAN =
+    "$2y$10$zqMVt81TW.ARWAQIWBRBS./9/xbdLkQp3SXwN5ISWqrxqqyUTx2AC";
+export const NAMA_API_AUTH =
+    "$2y$10$qtz8CPgtdfZAgCcsiVlDBuGgxn7EO1hUbYKF3PWWUyP67xb07C2X.";
+export const NAMA_API_RAW_MATERIAL =
+    "$2y$10$p7HTiCbBWVt0xSa3SdTz5u4XgyGI7WC7XaFgE/Xv6y2JYikU1cCjK";
+export const NAMA_API_PURHCASE_ORDER =
+    "$2y$10$CqiNTH3ltIDG.W/vZuVi9uGBKmeCQP0rILwDYwx/cMd5l2jmqGHfG";
+export const NAMA_API_EXPEDITION =
+    "$2y$10$zjWwR9gobHCEpjlvu2KWhegoaulYvt5v3c/n4MWVJd3Qbrn0U8alm";
+
+export const kodePerusahaan = "001";
+export const versionApp = "1.0.0";
